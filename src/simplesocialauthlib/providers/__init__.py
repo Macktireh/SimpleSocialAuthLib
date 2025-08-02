61bca097217a1eb1ab70f30ce8335107b0299be5
Macktireh <abdimack97@gmail.com>
2025-08-02 15:13:59 +0200

    Refactor provider imports in __init__.py to handle ImportError and improve dependency management

--- src/simplesocialauthlib/providers/__init__.py
+++ src/simplesocialauthlib/providers/__init__.py
@@ -1,4 +1,35 @@
-from simplesocialauthlib.providers.github import GithubSocialAuth
-from simplesocialauthlib.providers.google import GoogleSocialAuth
+__all__ = []
 
-__all__ = ["GithubSocialAuth", "GoogleSocialAuth"]
+try:
+    from simplesocialauthlib.providers.github import GithubSocialAuth
+
+    __all__.append("GithubSocialAuth")
+except ImportError:
+    pass
+
+try:
+    from simplesocialauthlib.providers.google import GoogleSocialAuth
+
+    __all__.append("GoogleSocialAuth")
+except ImportError:
+    pass
+
+
+def _importerror_factory(provider_name: str, install_command: str):
+    def raiser(*args, **kwargs):
+        raise ImportError(
+            f"The '{provider_name}' provider is not available because its dependencies are not installed.\n"  # noqa: E501
+            f"Please run: pip install {install_command}"
+        )
+
+    return raiser
+
+
+if "GithubSocialAuth" not in __all__:
+    GithubSocialAuth = _importerror_factory("github", "simplesocialauthlib[github]")
+    __all__.append("GithubSocialAuth")
+
+
+if "GoogleSocialAuth" not in __all__:
+    GoogleSocialAuth = _importerror_factory("google", "simplesocialauthlib[google]")
+    __all__.append("GoogleSocialAuth")
